43ede3cfbc0f6874b690a516ec5fd229bdaaebed
Shuiloon Yip <shuoodle@gmail.com>
2017-04-06 21:36:48 +0100

    Add basic todo features

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+$('.list').on('click', '.list__item', function() {
+  $(this).toggleClass('list__item_done');
+});
+
+$('.list').on('click', '.list__bin', function(event) {
+  $(this).parent().fadeOut(function() {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$('.list__input').on('keypress', function(event) {
+  if (event.which === 13) {
+    var inputText = $(this).val();
+    $('.list').append('<li class="list__item"><span class="list__bin">x </span>' + inputText + '</li>');
+    $(this).val('');
+  }
+});
