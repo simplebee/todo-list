c1c121fbb39e1770967a51811932036dd0585ae0
Shuiloon Yip <shuoodle@gmail.com>
2017-04-07 17:46:57 +0100

    Add color scheme

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ $('.list').on('click', '.list__bin', function(event) {
 $('.list__input').on('keypress', function(event) {
   if (event.which === 13) {
     var inputText = $(this).val();
-    $('.list').append('<li class="list__item"><span class="list__bin">x </span>' + inputText + '</li>');
+    $('.list').append('<li class="list__item"><span class="list__bin"><i class="fa fa-trash"></i></span>' + inputText + '</li>');
     $(this).val('');
   }
 });
